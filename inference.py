6176f2ff0445781829db5834ff3abf03aeadaa7d
jsdysw <jsdysw@gmail.com>
2024-01-25 08:21:50 +0000

    fix: load model from ckpt

--- inference.py
+++ inference.py
@@ -18,12 +18,12 @@ torch.cuda.manual_seed_all(SEED)
 
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 BASE_DIR = os.getcwd()
-DATA_DIR = os.path.join(BASE_DIR, '../data')
-OUTPUT_DIR = os.path.join(BASE_DIR, '../output')
+DATA_DIR = os.path.join(BASE_DIR, './data')
+MODEL_DIR = os.path.join(BASE_DIR, './output/checkpoint-2100')
 
 def test():  
     model_name = 'klue/bert-base'
-    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=7).to(DEVICE)
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_DIR).to(DEVICE)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     
     dataset_test = pd.read_csv(os.path.join(DATA_DIR, 'test.csv'))
@@ -39,8 +39,6 @@ def test():
             
     dataset_test['target'] = preds
     dataset_test.to_csv(os.path.join(BASE_DIR, 'output.csv'), index=False)
-            
-    dataset_test.head()
-    
+                
 if __name__ == '__main__':
     test()
