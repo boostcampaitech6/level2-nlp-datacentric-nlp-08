f58021a3713605f1b61aa1448a37e228e092869d
exena <exena01@gmail.com>
2024-01-31 14:30:52 +0900

    Put whole data into the tokenizer at once

--- dataset.py
+++ dataset.py
@@ -27,17 +27,18 @@ class BERTDataset(Dataset):
 class BERTTestDataset(Dataset):
     def __init__(self, data, tokenizer):
         input_texts = data['text']
-        self.inputs = []
+        # self.inputs = []
         
-        for text in input_texts:
-            tokenized_input = tokenizer(text, padding='max_length', max_length=128, truncation=True, return_tensors='pt')
-            self.inputs.append(tokenized_input)
+        self.tokenized_inputs = tokenizer(input_texts.tolist(), padding=True, return_tensors='pt')
+        # for text in input_texts:
+        #     tokenized_input = tokenizer(text, padding='max_length', max_length=128, truncation=True, return_tensors='pt')
+        #     self.inputs.append(tokenized_input)
             
     def __getitem__(self, idx):
         return {
-            'input_ids': self.inputs[idx]['input_ids'].squeeze(0),  
-            'attention_mask': self.inputs[idx]['attention_mask'].squeeze(0)
+            'input_ids': self.tokenized_inputs['input_ids'][idx].squeeze(0),  
+            'attention_mask': self.tokenized_inputs['attention_mask'][idx].squeeze(0)
         }
     
     def __len__(self):
-        return len(self.inputs)
+        return len(self.tokenized_inputs['input_ids'])
